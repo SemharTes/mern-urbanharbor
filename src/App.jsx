0b90c39e5a7282698b4c1859bed7daa56737ba53
Semhar Tes <semhartes7@gmail.com>
2023-12-03 15:47:26 -0500

    Created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
 function App() {
-  return <div className="text-red-950">MERN template!</div>;
+  return <BrowserRouter >
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/sign-up" element={<SignUp />}></Route>
+      <Route path="/sign-in" element={<SignIn/>}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+  </BrowserRouter>;
 }
 export default App;
