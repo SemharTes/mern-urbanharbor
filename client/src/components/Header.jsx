36e681b7bb457c1f807a02b49bbe593061025a62
Semhar Tes <semhartes7@gmail.com>
2023-12-12 12:36:39 -0500

    create user model

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-slate-300 shadow-md">
+    <header className="bg-violet-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-warp">
